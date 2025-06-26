08e470e148008d07c70c42893335b170675d1cfc
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-26 12:14:10 +0530

    Fix the layout and index file location

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-dark pt-40">
+      <Text className="text-white">
+        This is a starter template with convex and clerk
+      </Text>
+    </View>
+  );
+}
--- src/app/̐(app)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 bg-dark pt-40">
-      This is a starter template with convex and clerk
-    </View>
-  );
-}
