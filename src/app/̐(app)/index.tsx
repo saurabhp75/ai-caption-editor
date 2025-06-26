3b0b9bc624deb91ed79985c8931b85d1b65efa5c
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-26 12:05:43 +0530

    Cleanup the starter template

--- src/app/̐(app)/index.tsx
+++ src/app/̐(app)/index.tsx
@@ -1,17 +1,9 @@
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
-  const tasks = useQuery(api.tasks.get);
-
   return (
     <View className="flex-1 bg-dark pt-40">
-      {tasks?.map(({ _id, text }) => (
-        <Text className="text-white" key={_id}>
-          {text}
-        </Text>
-      ))}
+      This is a starter template with convex and clerk
     </View>
   );
 }
